3774ac4aa8be4f00cdd8a65bfc0f1b430c3366e0
cjurjiu <cjurjiu@users.noreply.github.com>
2018-05-14 18:59:17 +0200

    Add bookmark DB Model & Dao.
    Type: New Feature

---
+++ app/src/main/kotlin/com/catalinj/cryptosmart/datalayer/database/dao/BookmarksDao.kt
@@ -0,0 +1,28 @@
+package com.catalinj.cryptosmart.datalayer.database.dao
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Query
+import com.catalinj.cryptosmart.datalayer.database.models.DbBookmark
+import com.catalinj.cryptosmart.datalayer.database.models.DbCryptoCoin
+import com.catalinj.cryptosmart.datalayer.database.models.DbPartialCryptoCoin
+import io.reactivex.Flowable
+
+/**
+ * Created by catalin on 11/05/2018.
+ */
+@Dao
+interface BookmarksDao {
+
+    @Query("SELECT * FROM ${DbBookmark.BOOKMARKS_TABLE_NAME}")
+    fun getBookmarks(): List<DbBookmark>
+
+    @Query("SELECT * FROM ${DbBookmark.BOOKMARKS_TABLE_NAME}")
+    fun getBookmarksFlowable(): Flowable<List<DbBookmark>>
+
+    @Query("SELECT * FROM ${DbPartialCryptoCoin.COIN_TABLE_NAME}" +
+            " INNER JOIN ${DbPartialCryptoCoin.COIN_TABLE_NAME}" +
+            " ON ${DbBookmark.BOOKMARKS_TABLE_NAME}.${DbBookmark.ColumnNames.BOOKMARKED_COIN_SYMBOL} = " +
+            "${DbPartialCryptoCoin.COIN_TABLE_NAME}.${DbPartialCryptoCoin.ColumnNames.SYMBOL}" +
+            " ORDER BY ${DbBookmark.BOOKMARKS_TABLE_NAME}.${DbBookmark.ColumnNames.DATE_ADDED} DESC")
+    fun getBookmarkedCoins(): List<DbCryptoCoin>
+}
